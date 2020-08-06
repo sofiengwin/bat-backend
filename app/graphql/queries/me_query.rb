2391b1d3689448393c7c93b791962bd4d8ff64ae
Ogbara Godwin <godwin.onisofien@andela.com>
2020-08-06 03:01:08 +0100

    change admin type for user type in mequery

--- app/graphql/queries/me_query.rb
+++ app/graphql/queries/me_query.rb
@@ -1,6 +1,6 @@
 module Queries
   class MeQuery < BaseResolver
-    type Types::AdminType, null: true
+    type Types::UserType, null: true
 
     def resolve
       context[:current_user] || GraphQL::ExecutionError.new("Something went wrong", extensions: { "admin" => "notAuthorized" })
