c3c237658c9346909eb8cc9e7bb578498759da44
Ogbara Godwin <godwin.onisofien@andela.com>
2020-08-08 15:03:19 +0100

    add id to tip type

--- app/graphql/types/tip_type.rb
+++ app/graphql/types/tip_type.rb
@@ -1,5 +1,6 @@
 module Types
   class TipType < BaseObject
+    field :id, Int, null: false
     field :rating, String, null: true
     field :outcome, String, null: false
     field :bet, String, null: false
