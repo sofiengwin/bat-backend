931295048e8c6b52b2ae3eca2ddd16bd85301f2b
Ogbara Godwin <godwin.onisofien@andela.com>
2020-09-03 23:53:05 +0100

    make match.score nullable

--- app/graphql/types/match_type.rb
+++ app/graphql/types/match_type.rb
@@ -3,7 +3,7 @@ module Types
     field :id, ID, null: false
     field :homeTeamName, String, null: false
     field :awayTeamName, String, null: false
-    field :score, String, null: false
+    field :score, String, null: true
     field :league, String, null: false
     field :country, String, null: false
   end 
