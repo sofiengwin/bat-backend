28dc61fd8108bfc474f09ad428abb4eeb7290c62
Ogbara Godwin <godwin.onisofien@andela.com>
2020-09-05 13:42:38 +0100

    finally fix order by issue

--- app/services/filter_tips.rb
+++ app/services/filter_tips.rb
@@ -16,7 +16,7 @@ class FilterTips < Service::Base
   end
 
   private def approved_tips
-    Tip.where.not(approved_at: nil).order(created_at: :desc)
+    Tip.where.not(approved_at: nil).order('tips.created_at desc')
   end
 
   private def filter_by_odd(relation)
