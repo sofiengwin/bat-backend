919b848f51657a218154e84bc9c72c6f3a9017a4
Ogbara Godwin <godwin.onisofien@andela.com>
2020-09-05 14:21:23 +0100

    debug 6

--- app/services/filter_tips.rb
+++ app/services/filter_tips.rb
@@ -10,7 +10,7 @@ class FilterTips < Service::Base
   end
 
   def perform
-    @tips = filter_already_in_accumulation(filter_by_country(filter_bet_type(filter_by_odd((approved_tips))))).limit(30).reverse_order
+    @tips = filter_already_in_accumulation(filter_by_country(filter_bet_type(filter_by_odd((approved_tips))))).limit(30)
     pp @tips.map(&:created_at)
     Service::Result.resolve(@tips)
   end
