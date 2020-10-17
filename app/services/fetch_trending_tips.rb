fea1d9c2e67843e43dff00b34d93b134c22dfd99
sofiengwin <godwin.onisofien@andela.com>
2020-10-17 23:44:52 +0100

    remove unused query

--- app/services/fetch_trending_tips.rb
+++ app/services/fetch_trending_tips.rb
@@ -1,6 +1,4 @@
 class FetchTrendingTips < Service::Base
-  QUERY = "SELECT *, (SELECT COUNT(*) FROM tips WHERE tips.match_id  = m.id) AS tip_count FROM matches AS m ORDER BY tip_count DESC LIMIT 10"
-
   Trend = Struct.new(:home_team_name, :away_team_name, :country, :league, :tip_count, :match_id)
 
   def perform
