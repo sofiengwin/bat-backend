db35564e6f37724c13efbfdf8517c906b33e733f
sofiengwin <godwin.onisofien@andela.com>
2023-02-16 21:32:52 -0800

    improve seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,85 +19,91 @@
 #   username: 'Baddest bad guy'
 # )
 
+homes = ['Aston Villa', 'Brentford', 'Everton', 'Arsenal', 'Chelsea']
+aways = ['Manchester United', 'Manchester City', 'Westham', 'Leeds United', 'Fulham']
+bet_categories = ['1 x 2', 'Over 2.5', 'Double Chance', 'Halftime/Fulltime']
+bets = ['X', '1', '2', '1X', 'X2', 'Over.25']
+
 users = User.all
 
-4.times do |n|
+100.times do |n|
   match = Match.create(
-    home_team_name: 'Manchester United',
-    away_team_name: 'Chelsea',
+    home_team_name: homes.sample,
+    away_team_name: aways.sample,
     score: '2 - 2',
     start_at: Time.now,
-    fixture_id: n + 1,
+    fixture_id: rand(100000),
     league: 'Premier League',
     country: 'England'
   )
 
-  rand(1...10).times do
-    user = users.sample
-    tip = Tip.create(
-      user: user,
-      match: match,
-      outcome: 'won',
-      bet: '1X',
-      odd: 1.55,
-      mongo_id: '4994jjdfjjkdf-jiriuejd-394ew'
-    )
-
-    # Point.create(
-    #   value: 10,
-    #   tip: tip,
-    #   user: user
-    # )
-  end
+  user = users.sample
+  tip = Tip.create(
+    user: user,
+    match: match,
+    outcome: 'won',
+    bet: bets.sample,
+    bet_category: bet_categories.sample,
+    odd: 1.55,
+    mongo_id: '4994jjdfjjkdf-jiriuejd-394ew',
+    approved_at: Time.now
+  )
 end
 
 
-# SELECT *, (SELECT COUNT(*) FROM tips WHERE tips.match_id  = m.id) AS tip_count FROM matches AS m ORDER BY tip_count DESC
+# # SELECT *, (SELECT COUNT(*) FROM tips WHERE tips.match_id  = m.id) AS tip_count FROM matches AS m ORDER BY tip_count DESC
 
-mockUser = {
-	name: "King Tiper",
-	email: "king.tipper@gmail.com",
-	access_token: "fake.access.token",
-	token_id: "fake.token.id",
-	provider_id: "f",
-	avatar_url: "path/to/file.png"
-}
+# mockUser = {
+# 	name: "King Tiper",
+# 	email: "king.tipper@gmail.com",
+# 	access_token: "fake.access.token",
+# 	token_id: "fake.token.id",
+# 	provider_id: "f",
+# 	avatar_url: "path/to/file.png"
+# }
 
-[
-  {
-    provider: 'Betensured',
-    name: 'The Dark One',
-  },
-  {
-    provider: 'BetAdvice',
-    name: 'General Seer',
-  },
-  {
-    provider: 'Stats24',
-    name: 'Idasboki',
-  },
-  {
-    provider: 'AbrahamTips',
-    name: 'Doctor Strange',
-  },
-  {
-    provider: 'AFootballReport',
-    name: 'Alabo Don Pato',
-  },
-  {
-    provider: 'SoccerVista',
-    name: 'Martinez D King',
-  },
-  {
-    provider: 'olbg',
-    name: 'Baaed Bunt',
-  },
-  {
-    provider: 'ConfirmBets',
-    name: 'DBello',
-  },
-].each do |provider|
-  CreateUser.perform(**mockUser.merge(name: provider[:name], email: "#{provider[:provider]}@guru.com"), provider_id: rand(1...1000), approved_provider_at: Time.now)
-end
+# [
+#   {
+#     provider: 'Betensured',
+#     name: 'The Dark One',
+#   },
+#   {
+#     provider: 'BetAdvice',
+#     name: 'General Seer',
+#   },
+#   {
+#     provider: 'Stats24',
+#     name: 'Idasboki',
+#   },
+#   {
+#     provider: 'AbrahamTips',
+#     name: 'Doctor Strange',
+#   },
+#   {
+#     provider: 'AFootballReport',
+#     name: 'Alabo Don Pato',
+#   },
+#   {
+#     provider: 'SoccerVista',
+#     name: 'Martinez D King',
+#   },
+#   {
+#     provider: 'olbg',
+#     name: 'Baaed Bunt',
+#   },
+#   {
+#     provider: 'ConfirmBets',
+#     name: 'DBello',
+#   },
+# ].each do |provider|
+#   CreateUser.perform(**mockUser.merge(name: provider[:name], email: "#{provider[:provider]}@guru.com"), provider_id: rand(1...1000), approved_provider_at: Time.now)
+# end
 
 # AdminUser.create(email: 'admin@example.com', password: 'password')
+
+# users.each do |user|
+#   20.times do
+#     pp user.name
+#     user.user_point_counters.create({awarded_at: rand(100).days.ago, point: rand(50..100)})
+#   end
+# end
